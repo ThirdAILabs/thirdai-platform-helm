054642cf78445192b709fb8ae39c2fe1648d4525
Pratik Pranav <pratik@thirdai.com>
2025-01-30 16:34:07 +0530

    updates

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-Steps to create an EKS cluster:
-1. Create custom configuration EKS cluster (not auto). This is done with the assumption that a non-auto cluster is more similar to other cloud provider Kubernetes services
-2. Add the EBS and EFS CSI driver add-ons, in addition to the preselected add-ons
-3. Create IAM roles for the following service accounts:
-    1. Amazon VPC CNI
-        1. AmazonEKS_CNI_Policy
-    2. EBS CSI Driver
-        1. AmazonEBSCSIDriverPolicy
-    3. EFS CSI Driver
-        1. AmazonEFSCSIDriverPolicy
-4. Create node group with recommended IAM policy
-5. Run `aws eks update-kubeconfig --region <region-code> --name <my-cluster>` locally to populate your kubeconfig file with your eks cluster metadata
-6. Run `kubectl config use-context <context-name>` with your Kubernetes cluster context. You can run `kubectl config view` to see your contexts
-7. Run `kubectl create secret docker-registry docker-credentials-secret --docker-server=thirdaiplatform.azurecr.io --docker-username=thirdaiplatform-pull-release-test-main --docker-password='5Di/+qW2Q/++3mp0Ah/rkCq33n2N7f0E8G4+cSHnub+ACRClJvCj'` to add the docker credential secret to your runtime (this docker credential doesn't need to be a secret, but for now this is how we can add secrets in the CLI)
-8. Run `helm install thirdaiplatform .` in this repo's head directory to launch modelbazaar
+## ThirdAI Platform Terraform Installation and Helm Charts
--- terraform-scripts/eks-cluster.tf
+++ terraform-scripts/eks-cluster.tf
@@ -2,29 +2,26 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name       = var.cluster_name
-  cluster_version    = var.cluster_version
-  vpc_id             = module.vpc.vpc_id
-  subnet_ids         = module.vpc.private_subnets
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true  # You can keep private access enabled
-  cluster_endpoint_public_access_cidrs     = ["0.0.0.0/0"]  # Adjust CIDR for security
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+  
+  enable_cluster_creator_admin_permissions = true
 
-  # Enable IRSA for linking AWS IAM policies to K8s service accounts
+
   enable_irsa = true
 
-  # Define only the add-ons you want to explicitly manage versions/policies for
+  # Example cluster addons
   cluster_addons = {
-    coredns = {
-      # optional override for coredns version if desired
-    }
-
-    kube-proxy = {
-      # optional override for kube-proxy version if desired
-    }
-
-    vpc-cni = {
+    coredns   = {}
+    kube-proxy = {}
+    vpc-cni   = {
       version                     = "latest"
       resolve_conflicts           = "OVERWRITE"
       service_account_role_name   = "${var.cluster_name}-vpc-cni-irsa-role"
@@ -32,27 +29,23 @@ module "eks" {
         "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
       ]
     }
-
-    # Corrected EBS CSI Add-On
     aws-ebs-csi-driver = {
       service_account_role_name   = "${var.cluster_name}-ebs-csi-irsa-role"
       service_account_role_policy_arns = [
         "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicy"
       ]
-      version = "latest"  # Or specify a specific version if needed
+      version = "latest"
     }
-
-    # Corrected EFS CSI Add-On
     aws-efs-csi-driver = {
       service_account_role_name   = "${var.cluster_name}-efs-csi-irsa-role"
       service_account_role_policy_arns = [
         "arn:aws:iam::aws:policy/AmazonEFSCSIDriverPolicy"
       ]
-      version = "latest"  # Or specify a specific version if needed
+      version = "latest"
     }
   }
 
-  # Node group and other configurations remain unchanged
+  # Example managed node group
   eks_managed_node_groups = {
     main = {
       name             = "${var.cluster_name}-node-group"
@@ -71,5 +64,13 @@ module "eks" {
   tags = {
     "Project" = var.cluster_name
   }
+}
 
+# Grab cluster data so we can configure the Kubernetes provider
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
 }

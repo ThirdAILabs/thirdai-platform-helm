6b92fc55017a7aa42fc31070b8794f213e15d80f
Pratik Pranav <pratik@thirdai.com>
2025-01-31 20:43:03 +0530

    works with nfs

--- terraform-scripts/vpc.tf
+++ terraform-scripts/vpc.tf
@@ -20,22 +20,30 @@ module "vpc" {
 data "aws_availability_zones" "available" {}
 
 
-resource "aws_security_group_rule" "allow_nfs_ingress" {
-  type              = "ingress"
-  from_port         = 2049
-  to_port           = 2049
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = module.vpc.default_security_group_id
-  description       = "Allow NFS traffic from EKS nodes to EFS"
+resource "aws_security_group" "efs_sg" {
+  name        = "efs-sg"
+  description = "Security group for EFS mount targets"
+  vpc_id      = module.vpc.vpc_id # or your VPC's ID
+
+  tags = {
+    Name = "my-efs-sg"
+  }
 }
 
-resource "aws_security_group_rule" "allow_nfs_egress" {
-  type              = "egress"
-  from_port         = 2049
-  to_port           = 2049
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = module.vpc.default_security_group_id
-  description       = "Allow NFS traffic from EKS nodes to EFS"
+resource "aws_vpc_security_group_ingress_rule" "efs_sg_ingress" {
+  security_group_id = aws_security_group.efs_sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 0
+  ip_protocol = "-1"
+  to_port     = 0
+}
+
+resource "aws_vpc_security_group_egress_rule" "efs_sg_egress" {
+  security_group_id = aws_security_group.efs_sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 0
+  ip_protocol = "-1"
+  to_port     = 0
 }
